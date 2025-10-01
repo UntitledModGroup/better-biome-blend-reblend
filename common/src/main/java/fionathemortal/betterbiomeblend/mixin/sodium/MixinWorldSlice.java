0ce0808d5bd3e2774711ec5f114903f0879df2e8
Cart <81428538+cartrigger@users.noreply.github.com>
2025-09-30 23:43:13 -0400

    Fix
    Co-authored-by: AlphaMode <alphamodeyt@gmail.com>

--- common/src/main/java/fionathemortal/betterbiomeblend/mixin/sodium/MixinWorldSlice.java
+++ common/src/main/java/fionathemortal/betterbiomeblend/mixin/sodium/MixinWorldSlice.java
@@ -1,9 +1,10 @@
 package fionathemortal.betterbiomeblend.mixin.sodium;
 
 import me.jellysquid.mods.sodium.client.world.WorldSlice;
+import me.jellysquid.mods.sodium.client.world.biome.BiomeColorSource;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.ColorResolver;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,11 +12,25 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = WorldSlice.class, remap = false)
-public abstract class MixinWorldSlice implements BlockAndTintGetter {
+public abstract class MixinWorldSlice {
     @Shadow(remap = false) @Final private ClientLevel world;
 
     /**
-     * @author icanttellyou
+     * @author AlphaMode
+     * @reason Use vanilla method
+     */
+    @Overwrite
+    public int getColor(BiomeColorSource source, int x, int y, int z) {
+        BlockPos pos = new BlockPos(x, y, z);
+        return switch (source) {
+            case GRASS -> getBlockTint(pos, BiomeColors.GRASS_COLOR_RESOLVER);
+            case FOLIAGE -> getBlockTint(pos, BiomeColors.FOLIAGE_COLOR_RESOLVER);
+            case WATER -> getBlockTint(pos, BiomeColors.WATER_COLOR_RESOLVER);
+        };
+    }
+
+    /**
+     * @author AlphaMode
      * @reason Use vanilla method
      */
     @Overwrite
