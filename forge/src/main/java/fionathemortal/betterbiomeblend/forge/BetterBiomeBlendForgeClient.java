4c0238f3843d067729489fbf34043f18eccc2f09
Cart <81428538+cartrigger@users.noreply.github.com>
2025-10-01 02:23:04 -0400

    Forge Works Now

--- forge/src/main/java/fionathemortal/betterbiomeblend/forge/BetterBiomeBlendForgeClient.java
+++ forge/src/main/java/fionathemortal/betterbiomeblend/forge/BetterBiomeBlendForgeClient.java
@@ -1,15 +1,11 @@
 package fionathemortal.betterbiomeblend.forge;
 
 import com.mojang.brigadier.CommandDispatcher;
-import fionathemortal.betterbiomeblend.BetterBiomeBlend;
 import fionathemortal.betterbiomeblend.BetterBiomeBlendClient;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = BetterBiomeBlend.MOD_ID, value = Dist.CLIENT)
 public class BetterBiomeBlendForgeClient
 {
     @SubscribeEvent
