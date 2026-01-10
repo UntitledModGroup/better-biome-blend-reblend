ed8981c34268300e8321ce15e4c325f1e4e992f5
icanttellyou <cuboidedposition32@protonmail.com>
2026-01-10 17:27:00 +0200

    chore: Remove weird IntelliJ screwup

--- common/src/main/java/fionathemortal/betterbiomeblend/BetterBiomeBlendClient.java
+++ common/src/main/java/fionathemortal/betterbiomeblend/BetterBiomeBlendClient.java
@@ -10,7 +10,7 @@ import net.minecraft.client.Options;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;import java.util.Optional;
+import net.minecraft.world.entity.player.Player;
 
 public final class BetterBiomeBlendClient
 {
